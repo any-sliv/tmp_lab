feab6e56b0b31f111e2662676a26bfc4fd4b224c
Maciej Śliwiński <mjsliwinski@outlook.com>
2021-01-22 05:16:39 +0200

    seg fault

--- uiodriver.c
+++ uiodriver.c
@@ -36,17 +36,6 @@ int main()
         // Redirect stdout/printf into the /dev/kmsg file 
         // (it will be possible to print using printk)
         freopen ("/dev/kmsg","w",stdout);
-<<<<<<< HEAD
-=======
- 
-        printf("2");
-
-
-        // get architecture specific page size
-        unsigned pageSize = sysconf(_SC_PAGESIZE);
->>>>>>> master
-
-        return;
 
         /**********************************************************************
          * TASK 1: Write a SINGLE LINE COMMAND to map the physical address    *
@@ -60,14 +49,6 @@ int main()
         *         UIO" section in Lab 3 additional materials for details.    *
         **********************************************************************/
 
-<<<<<<< HEAD
-=======
-        // WRITE YOUR SINGLE LINE COMMAND IN HERE //////////////////////////////
-        
-        printf("before mapping");
-        ptr = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-        printf("Mapped");
->>>>>>> master
 
         ptr = mmap(NULL, 4, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
